915b2364dcf49c62ae5d964d33f4023d0572fd6c
anhvipx266 <61503224+anhvipx266@users.noreply.github.com>
2023-02-17 17:25:21 +0700

    Update bai 1.cpp

--- c/bai tap/buoi 7/bai 1.cpp
+++ c/bai tap/buoi 7/bai 1.cpp
@@ -3,14 +3,35 @@
 
 int main(){
 	
-	int cap;
+	int cap,i;
+	int eisum = 0; 
+	
+	
 	printf("Nhap vao kich thuoc: ");
 	scanf("%d",&cap);
 	int a[cap];
-	for(int i=0;i<cap;i++){
+	for(i=0;i<cap;i++){
 		printf("Nhap vao phan tu %d: ");
 		scanf("%d",&a[i]);
 	}
-	
+	// in ra cac gia tri vua nhap 
+	printf("Gia tri cua cac phan tu vua nhap:\n");
+	for(i=0;i<cap;i++)
+		printf("[%d] = %d\n",i+1,a[cap]);
+	// tong cac gia tri co chi so chan
+	for(i=0;i<cap;i+=2)
+		eisum+=a[i];
+	printf("\nTong cac phan tu chi so chan la: %d",eisum);
+	// in ra man hinh cac gia tri le
+	printf("\nCac phan tu le la:\n");
+	for(i=0;i<cap;i++){
+		if(a[cap]%2==1){
+			printf("[%d]=%d\n",i+1,a[i]);
+		}
+	}
+	// 5.	In ra màn hình các phần tử có chỉ số (key) lẻ
+	printf("Cac phan tu co chi so le la:\n");
+	for(i=1;i<cap;i+=2)
+		printf("[%d]=%d\n",i,a[i]);
 	return 0;
 }
