7b3fdad8bb6bfde80c0b08fa9c2ff2e0abd1d8ca
himT <61503224+anhvipx266@users.noreply.github.com>
2024-05-29 16:34:43 +0700

    Add files via upload

---
+++ jsp/bkshop.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th5 29, 2024 lúc 11:33 AM
+-- Phiên bản máy phục vụ: 10.4.25-MariaDB
+-- Phiên bản PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `bkshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danhmuc`
+--
+
+CREATE TABLE `danhmuc` (
+  `MaDanhMuc` int(11) NOT NULL,
+  `TenDanhMuc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`MaDanhMuc`, `TenDanhMuc`) VALUES
+(1, 'Trà sữa'),
+(2, 'Cafe');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSanPham` int(11) NOT NULL,
+  `TenSanPham` varchar(255) NOT NULL,
+  `Gia` decimal(10,2) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `MoTa` text DEFAULT NULL,
+  `MaDanhMuc` int(11) DEFAULT NULL,
+  `Anh` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`MaSanPham`, `TenSanPham`, `Gia`, `SoLuong`, `MoTa`, `MaDanhMuc`, `Anh`) VALUES
+(1, 'Trà sữa chân châu', '20000.00', 234, 'Trà sữa chân châu đường đen', 1, 'https://file.hstatic.net/1000135323/file/tra_sua_ngon_0e87236e4d7442fb826c502798ec6f7e_1024x1024.jpg'),
+(2, 'Cà phê đá', '12000.00', 51, 'Cà phê đen đá', 2, 'https://th.bing.com/th/id/OIP.Lv10Y55e4HJmUt8cwC9B6wHaHa?rs=1&pid=ImgDetMain'),
+(3, 'Trà phê sữa', '25000.00', 1234, 'Trà phê sữa đá với kem', 2, 'https://coffeeaffection.com/wp-content/uploads/2022/07/oolong-milk-tea-in-a-glass_thangchuphotos.com_Shutterstock.jpg');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  ADD PRIMARY KEY (`MaDanhMuc`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSanPham`),
+  ADD KEY `MaDanhMuc` (`MaDanhMuc`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  MODIFY `MaDanhMuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `MaSanPham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`MaDanhMuc`) REFERENCES `danhmuc` (`MaDanhMuc`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
